0b8f7940ba52d665648fd65b40f02900bace9aa9
jrson83 <me@jrson.de>
2022-12-10 15:39:21 +0100

    updated fmt script & run fmt

--- server.d.ts
+++ server.d.ts
@@ -1,6 +1,6 @@
 import * as Inertia from '@inertiajs/core'
 
-export type AppCallback = (page: Inertia.Page) => Inertia.InertiaAppResponse;
-export function createServer(render: AppCallback, port?: number | undefined): void;
+export type AppCallback = (page: Inertia.Page) => Inertia.InertiaAppResponse
+export function createServer(render: AppCallback, port?: number | undefined): void
 
 export { default as createServer } from './dist/server'
